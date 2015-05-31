591036fe9a611cce03e40000aefad173a5ba6287
Alexandre Picogna <apicogna@enssat.fr>
2015-05-31 16:35:04 +0200

    modifications mineures

--- CodeIgniter/application/views/login_view.php
+++ CodeIgniter/application/views/login_view.php
@@ -6,11 +6,11 @@
         echo form_open('http://localhost/ENSSAT_Projet_Web1/CodeIgniter/index.php/loginVerification', 'role="form"', 'style=width:800px');
             echo '<div class="form-group col-lg-12">';
                 echo form_label('Login :', 'username');
-                echo form_input('login', set_value('login'), 'class="form-control"'). '<br/>';
+                echo form_input('login', set_value('login'), 'class="form-control" placeholder="Saisir votre login"'). '<br/>';
             echo '</div>';
             echo '<div class="form-group col-lg-12">';
                 echo form_label('Mot de passe :', 'pwd');
-                echo form_password('mdp','','class="form-control"'). '<br/><br/>';
+                echo form_password('mdp','','class="form-control" placeholder="Saisir votre mot de passe"'). '<br/><br/>';
             echo '</div>';
             echo '<div class="form-group col-lg-2">';
                 echo form_submit('submit', 'Connexion','class="btn btn-default"');
