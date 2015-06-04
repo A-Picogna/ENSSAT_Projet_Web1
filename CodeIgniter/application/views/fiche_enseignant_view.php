fe6f01241c485044fee8ac6ac4bb605c0bd72179
Yiserot <yserot.perso@gmail.com>
2015-06-04 16:42:34 +0200

    eq. TD

--- CodeIgniter/application/views/fiche_enseignant_view.php
+++ CodeIgniter/application/views/fiche_enseignant_view.php
@@ -15,23 +15,14 @@
 			$mod = $cours["module"];
 			echo "</table><table class='table table-striped'><tr><th colspan='2'>".$cours["libelle"]." - ".$cours["public"]."</th></tr>";
 		} 
-		echo "<tr><td>".$cours["partie"]."</td><td>".$cours["hed"]." h eq. ".$cours["type"]."</td></tr>";
+		echo "<tr><td>".$cours["partie"]."</td><td>".$cours["hed"]." h eq. TD</td></tr>";
 	}
 
 	echo "</table><table class='table table-striped'>";
 
-	if ($heuresCMEnseignant[0]['sum(hed)'] != '')
-		echo "<tr><td>Total</td><td>".$heuresCMEnseignant[0]['sum(hed)']." h eq. CM</tr></td>";
-	if ($heuresTDEnseignant[0]['sum(hed)'] != '')
-		echo "<tr><td>Total</td><td>".$heuresTDEnseignant[0]['sum(hed)']." h eq. TD</tr></td>";
-	if ($heuresTPEnseignant[0]['sum(hed)'] != null)
-		echo "<tr><td>Total</td><td>".$heuresTPEnseignant[0]['sum(hed)']." h eq. TP</tr></td>";
-	if ($heuresProjetEnseignant[0]['sum(hed)'] != null)
-		echo "<tr><td>Total</td><td>".$heuresProjetEnseignant[0]['sum(hed)']." h eq. Projet</tr></td>";
-
 	$total = ($heuresCMEnseignant[0]['sum(hed)'] + $heuresTDEnseignant[0]['sum(hed)'] + $heuresTPEnseignant[0]['sum(hed)'] + $heuresProjetEnseignant[0]['sum(hed)']);
 	if ($total == null)
 		$total = 0;
-	echo "<tr><td>Total</td><td>". $total ." h</tr></td></table>";
+	echo "<tr><td>Total</td><td>". $total ." h eq. TD</tr></td></table>";
 
 ?>
