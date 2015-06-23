23286bed1db5ddd1ae67b12b99706906bb4e8b6b
Yiserot <yserot.perso@gmail.com>
2015-06-23 13:43:18 +0200

    Fusion ajout et gestion module, suppression chosen

--- CodeIgniter/application/models/gestion_module_model.php
+++ CodeIgniter/application/models/gestion_module_model.php
@@ -6,6 +6,29 @@ class Gestion_Module_model extends CI_Model {
 		$this->load->database();
 	}
 
+	public function creer_module()
+	{
+		$resp = "";
+		if ($this->session->userdata('module')["idResponsable"] != "") {
+			$query = $this->db->query('Insert into module values ("'.$this->session->userdata('module')["ident"].'", "'.$this->session->userdata('module')["public"].'", "'.$this->session->userdata('module')["semestre"].'", "'.$this->session->userdata('module')["libelle"].'", "'.$this->session->userdata('module')["idResponsable"].'")');
+		}
+		else {
+			$query = $this->db->query('Insert into module values ("'.$this->session->userdata('module')["ident"].'", "'.$this->session->userdata('module')["public"].'", "'.$this->session->userdata('module')["semestre"].'", "'.$this->session->userdata('module')["libelle"].'", null)');
+		}
+		if (!empty($query)) {
+			foreach($this->session->userdata('moduleCours') as $cours) {
+				$ens = "";
+				if ($cours["idEnseignant"] != "") {
+					$query = $this->db->query('Insert into contenu values ("'.$this->session->userdata('module')["ident"].'", "'.$cours["partie"].'", "'.$cours["type"].'", "'.$cours["hed"].'", "'.$cours["idEnseignant"].'")');
+				}
+				else {
+					$query = $this->db->query('Insert into contenu values ("'.$this->session->userdata('module')["ident"].'", "'.$cours["partie"].'", "'.$cours["type"].'", "'.$cours["hed"].'", null)');
+				}
+			}
+		}	
+		return $query;
+	}
+
 	public function get_liste_modules()
 	{
 		$query = $this->db->query('Select * from module order by ident');
@@ -80,14 +103,14 @@ class Gestion_Module_model extends CI_Model {
 		return $query->result_array();
 	}
 
-	public function verif_count_cours($ident) {
-		$query = $this->db->query('Select count(*) from contenu where module="'.$ident.'"');
-		return $query->result_array()[0];
-	}
-
 	public function verif_redondance_partie($ident, $str) {
 		$query = $this->db->query('Select partie from contenu where module="'.$ident.'" and partie="'.$str.'"');
 		return $query->result_array();
+	}
+
+	public function verif_count_cours($ident) {
+		$query = $this->db->query('Select count(*) from contenu where module="'.$ident.'"');
+		return $query->result_array()[0];
 	}
 }
 ?>
